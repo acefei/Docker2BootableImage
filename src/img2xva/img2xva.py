7283f6b82f736aa19ee226ae193282964d607265
acefei <acefei@163.com>
2023-03-03 17:29:11 +0800

    feat: convert cloud image from HTTP link

--- src/img2xva/img2xva.py
+++ src/img2xva/img2xva.py
@@ -6,13 +6,13 @@ Build an XVA using the new XVA format (version 2)
 
 import logging
 import os
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
 from errno import ENXIO
 from hashlib import sha1
 from math import ceil
 from pathlib import Path
 from string import Template
-from subprocess import check_call
+from subprocess import check_call, check_output
 from tempfile import TemporaryDirectory
 
 
@@ -95,9 +95,63 @@ def chunk_img(img, output_dir, chunk_size=1024 * 1024):
     return (size, chunk_list)
 
 
+def download_image(img):
+    logging.info(f"Downloading image {img}")
+    check_call(["wget", "-q", "--show-progress", img])
+
+
+def convert_image(qcow2, image_name):
+    logging.info(f"Convert {qcow2} to {image_name}")
+    check_call(["qemu-img", "convert", qcow2, image_name])
+
+
+def check_file_format(img):
+    file_format = check_output(["file", img]).decode()
+    if "QCOW2" in file_format:
+        return "QCOW2"
+    if "DOS/MBR" in file_format:
+        return "RAW"
+    return file_format
+
+
+def handle_image(img):
+    file_suffix = Path(img).suffix
+    if file_suffix not in [".raw", ".img", ".qcow2", "image"]:
+        raise Exception(
+            f"Invalid image {file_suffix}, only support qcow2 and raw image now."
+        )
+
+    output_image = None
+    image_basename = Path(img).name
+    if img[0:4] == "http":
+        download_image(img)
+
+    file_format = check_file_format(image_basename)
+    if file_format not in ["QCOW2", "RAW"]:
+        raise Exception(
+            f"Invalid file format: {file_format}, only support qcow2 and raw image now."
+        )
+
+    if file_format == "QCOW2":
+        output_image = Path(image_basename).with_suffix(".image").name
+        convert_image(image_basename, output_image)
+
+    return output_image
+
+
 def _produce_parser():
-    parser = ArgumentParser(description="Create an XVA from a sparse file. ")
-    parser.add_argument("image", help="Path of Raw image")
+    parser = ArgumentParser(
+        formatter_class=RawTextHelpFormatter,
+        description="Build XVA image from qcow2 or raw image ",
+    )
+    parser.add_argument(
+        "image",
+        help="""Path of cloud image, only support qcow2 and raw image, you can also use the existed cloud image whose link as follows:
+    Ubuntu: https://cloud-images.ubuntu.com/
+    Debian: https://cloud.debian.org/cdimage/cloud/
+    AmaLinux: https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/
+    """,
+    )
     parser.add_argument(
         "-c", "--cpus", type=int, default=2, help="CPU Number of VM. Default is 2"
     )
@@ -115,19 +169,19 @@ def main():
     log_level = logging.DEBUG if settings.verbose else logging.INFO
     logging.basicConfig(level=log_level, format=("[%(levelname)s] %(message)s"))
     script_dir = Path(__file__).resolve().parent
-    image_path = settings.image
-    image_name = Path(image_path).stem
+    image_name = handle_image(settings.image)
+    output_xva_name = Path(image_name).with_suffix(".xva")
 
     with TemporaryDirectory() as tempdir:
         root_vdi_ref = "Ref:VDI-1-root"
-        logging.info(f"Chunking {image_path}...")
+        logging.info(f"Chunking {image_name}...")
         (root_size_bytes, root_chunks) = chunk_img(
-            img=image_path, output_dir=f"{tempdir}/{root_vdi_ref}"
+            img=image_name, output_dir=f"{tempdir}/{root_vdi_ref}"
         )
 
         config = {
             # vm name with random string suffix
-            "vm_name_label": f"{image_name}-{tempdir[8:]}",
+            "vm_name_label": f"{image_name[:-4]}-{tempdir[8:]}",
             "vm_name_description": image_name,
             "memory_bytes": settings.memory * 1024 * 1024 * 1024,
             "vcpus": settings.cpus,
@@ -143,7 +197,7 @@ def main():
             ova = Template(fin.read())
             fout.write(ova.substitute(config))
 
-        xva_path = f"{script_dir}/{image_name}.xva"
+        xva_path = f"{script_dir}/{output_xva_name}"
         logging.info(f"Creating {xva_path}...")
         check_call(
             ["tar", "zchfP", xva_path, "--transform", f"s~{tempdir}/~~", ova_xml_path]
--- src/img2xva/run.sh
+++
@@ -1,4 +0,0 @@
-qcow2=${1?invalid input raw image}
-qcow2_name=${qcow2%%.*}
-qemu-img convert $qcow2 ${qcow2_name}.img
-python3 img2xva.py ${qcow2_name}.img
